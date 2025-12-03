e490d4f589a49d3a013210dabfa690a8cce767ff
Berkay Hasip <Berkay Hasip@DESKTOP-0RCRUMN>
2025-12-03 23:43:59 +0100

    Proje dosyası ekle.

---
+++ NetzwerkDiagnoseTool/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace NetzwerkDiagnoseTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.btnPing = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(40, 58);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(198, 20);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.TextChanged += new System.EventHandler(this.txtHost_TextChanged);
+            // 
+            // btnPing
+            // 
+            this.btnPing.Location = new System.Drawing.Point(40, 119);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(75, 23);
+            this.btnPing.TabIndex = 1;
+            this.btnPing.Text = "Send Ping";
+            this.btnPing.UseVisualStyleBackColor = true;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(40, 180);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(684, 96);
+            this.rtbOutput.TabIndex = 2;
+            this.rtbOutput.Text = "";
+            this.rtbOutput.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnPing);
+            this.Controls.Add(this.txtHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Button btnPing;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ NetzwerkDiagnoseTool/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+
+namespace NetzwerkDiagnoseTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHost_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnPing_Click(object sender, EventArgs e)
+        {
+            // Get the hostname/IP from the text box.
+            string host = txtHost.Text;
+
+            // Validate Check if the text box is empty or whitespace.
+            if (string.IsNullOrWhiteSpace(host))
+            {
+                //Show error message in the output box.
+                rtbOutput.AppendText("Error: Please enter a valid address (e.g. google.com)\n");
+                return; //Stop execution. 
+            }
+
+            //Inform user that the process has started.
+            rtbOutput.AppendText($"Pinging {host}...\n");
+
+            // Create a new Ping object instance.
+            Ping myPingSender = new Ping();
+
+            try
+            {
+                //Send ping asynchronously and await the reply. 1000ms timeout.
+                PingReply reply = await myPingSender.SendPingAsync(host, 1000);
+
+                // Check if the ping was successful.
+                if (reply.Status == IPStatus.Success)
+                {
+                 
+                    rtbOutput.AppendText($"Success! IP: {reply.Address}\n");
+
+                    rtbOutput.AppendText($"Time: {reply.RoundtripTime} ms\n");
+
+                    rtbOutput.AppendText("--------------------\n");
+                }
+                else
+                {
+                    rtbOutput.AppendText($"Failed. Status: {reply.Status}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                rtbOutput.AppendText($"System Error: {ex.Message}\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
